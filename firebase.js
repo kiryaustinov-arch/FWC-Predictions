8c64bf65db7d743173931b2f7c0c122b7727639c
kiryaustinov-arch <kirya.ustinov@gmail.com>
2026-06-11 14:55:31 +0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,1 +1,22 @@
+// firebase.js
 
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-app.js";
+
+import {
+    getFirestore
+} from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB7zwscFRdQl8Izk522LLesgbAG_8YqNlQ",
+  authDomain: "worldcuppredictor2026-c9339.firebaseapp.com",
+  projectId: "worldcuppredictor2026-c9339",
+  storageBucket: "worldcuppredictor2026-c9339.firebasestorage.app",
+  messagingSenderId: "265647409165",
+  appId: "1:265647409165:web:0bc0e7ed72ffa81e6fed44"
+};
+
+const app = initializeApp(firebaseConfig);
+
+const db = getFirestore(app);
+
+export { db };
