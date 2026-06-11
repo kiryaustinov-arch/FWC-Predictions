5a1538a5b5c3dc221b643f89d00f91aeb28f5949
kiryaustinov-arch <kirya.ustinov@gmail.com>
2026-06-11 15:21:27 +0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,12 +1,12 @@
-// Подключение Firebase
+// Firebase configuration
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB7zwscFRdQl8Izk522LLesgbAG_8YqNlQ",
-  authDomain: "worldcuppredictor2026-c9339.firebaseapp.com",
-  projectId: "worldcuppredictor2026-c9339",
-  storageBucket: "worldcuppredictor2026-c9339.firebasestorage.app",
-  messagingSenderId: "265647409165",
-  appId: "1:265647409165:web:0bc0e7ed72ffa81e6fed44"
+    apiKey: "AIzaSyB7zwscFRdQl8Izk522LLesgbAG_8YqNlQ",
+    authDomain: "worldcuppredictor2026-c9339.firebaseapp.com",
+    projectId: "worldcuppredictor2026-c9339",
+    storageBucket: "worldcuppredictor2026-c9339.firebasestorage.app",
+    messagingSenderId: "265647409165",
+    appId: "1:265647409165:web:0bc0e7ed72ffa81e6fed44"
 };
 
 firebase.initializeApp(firebaseConfig);
